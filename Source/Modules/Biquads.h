001f23377b17085d886d963f8802f8e1a400b471
Nathan Hood <nathanjhood@googlemail.com>
2022-06-03 17:08:06 +0200

    Used latest Biquads build files

--- Source/Modules/Biquads.h
+++ Source/Modules/Biquads.h
@@ -17,29 +17,29 @@
 
 enum class FilterType
 {
-    lowPass2,
-    lowPass1,
-    highPass2,
-    highPass1,
-    bandPass,
-    bandPassQ,
-    lowShelf2,
-    lowShelf1,
-    lowShelf1C,
-    highShelf2,
-    highShelf1,
-    highShelf1C,
-    peak,
-    notch,
-    allPass
+    lowPass2 = 0,
+    lowPass1 = 1,
+    highPass2 = 2,
+    highPass1 = 3,
+    bandPass = 4,
+    bandPassQ = 5,
+    lowShelf2 = 6,
+    lowShelf1 = 7,
+    lowShelf1C = 8,
+    highShelf2 = 9,
+    highShelf1 = 10,
+    highShelf1C = 11,
+    peak = 12,
+    notch = 13,
+    allPass = 14
 };
 
 enum class TransformationType
 {
-    directFormI,
-    directFormII,
-    directFormItransposed,
-    directFormIItransposed
+    directFormI = 0,
+    directFormII = 1,
+    directFormItransposed = 2,
+    directFormIItransposed = 3
 };
 
 /**
@@ -153,12 +153,12 @@ private:
     SampleType directFormIITransposed(int channel, SampleType inputValue);
 
     //==============================================================================
-    SampleType b0() { return static_cast<SampleType>(b0_); }
-    SampleType b1() { return static_cast<SampleType>(b1_); }
-    SampleType b2() { return static_cast<SampleType>(b2_); }
-    SampleType a0() { return static_cast<SampleType>(a0_); }
-    SampleType a1() { return static_cast<SampleType>(a1_); }
-    SampleType a2() { return static_cast<SampleType>(a2_); }
+    SampleType getb0() { return static_cast<SampleType>(b0); }
+    SampleType getb1() { return static_cast<SampleType>(b1); }
+    SampleType getb2() { return static_cast<SampleType>(b2); }
+    SampleType geta0() { return static_cast<SampleType>(a0); }
+    SampleType geta1() { return static_cast<SampleType>(a1); }
+    SampleType geta2() { return static_cast<SampleType>(a2); }
 
     //==============================================================================
     /** Unit-delay objects. */
@@ -171,12 +171,12 @@ private:
 
     //==============================================================================
     /** Initialise the coefficient gains. */
-    SampleType b0_ = 1.0;
-    SampleType b1_ = 0.0;
-    SampleType b2_ = 0.0;
-    SampleType a0_ = 1.0;
-    SampleType a1_ = 0.0;
-    SampleType a2_ = 0.0;
+    SampleType b0 = 1.0;
+    SampleType b1 = 0.0;
+    SampleType b2 = 0.0;
+    SampleType a0 = 1.0;
+    SampleType a1 = 0.0;
+    SampleType a2 = 0.0;
 
     //==============================================================================
     /** Parameter Smoothers. */
@@ -194,7 +194,7 @@ private:
 
     filterType filtType = filterType::lowPass2;
     transformationType transformType = transformationType::directFormIItransposed;
-    
+
     //==============================================================================
     /** Initialise constants. */
     const SampleType zero = (0.0);
