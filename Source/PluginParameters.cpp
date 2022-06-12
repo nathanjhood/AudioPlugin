224133d3da921712e64f3678c9a8aa817262cd26
Nathan Hood <nathanjhood@googlemail.com>
2022-06-12 23:29:29 +0200

    cleaned up Parameters

--- Source/PluginParameters.cpp
+++ Source/PluginParameters.cpp
@@ -13,7 +13,6 @@
 
 Parameters::Parameters(AudioPluginAudioProcessor& p, APVTS& apvts) : audioProcessor(p), state(apvts)
 {
-
 }
 
 void Parameters::setParameterLayout(Params& params)
@@ -22,54 +21,39 @@ void Parameters::setParameterLayout(Params& params)
     const auto dBMin = juce::Decibels::gainToDecibels(0.0625f);
     const auto dBOut = juce::Decibels::gainToDecibels(0.5f, -120.0f) * 20.0f;
 
-    const auto freqRange = juce::NormalisableRange<float>(20.00f, 20000.00f, 0.001f, 00.198894f);
-    const auto resRange = juce::NormalisableRange<float>(00.00f, 1.00f, 0.01f, 1.00f);
     const auto gainRange = juce::NormalisableRange<float>(dBMin, dBMax, 0.01f, 1.00f);
     const auto mixRange = juce::NormalisableRange<float>(00.00f, 100.00f, 0.01f, 1.00f);
     const auto outputRange = juce::NormalisableRange<float>(dBOut, dBMax, 0.01f, 1.00f);
 
-    const auto fString = juce::StringArray({ "LP2", "LP1", "HP2", "HP1" , "BP2", "BP2c", "LS2", "LS1c", "LS1", "HS2", "HS1c", "HS1", "PK2", "NX2", "AP2" });
-    const auto tString = juce::StringArray({ "DFI", "DFII", "DFI t", "DFII t" });
     const auto osString = juce::StringArray({ "--", "2x", "4x", "8x", "16x" });
+    const auto pString = juce::StringArray({ "floats", "doubles" });
 
-    const auto decibels =       juce::String    { ( "dB" ) };
-    const auto frequency =      juce::String    { ( "Hz" ) };
-    const auto reso =           juce::String    { ( "q"  ) };
-    const auto percentage =     juce::String    { ( "%"  ) };
-    const auto milliseconds =   juce::String    { ( ".ms" ) };
-    const auto inOut = juce::String{ ("IO") };
+    const auto decibels = juce::String { ( "dB" ) };
+    const auto frequency = juce::String { ( "Hz" ) };
+    const auto reso = juce::String { ( "q"  ) };
+    const auto percentage = juce::String { ( "%"  ) };
+    const auto milliseconds = juce::String { ( ".ms" ) };
+    const auto inOut = juce::String { ("IO") };
 
-    std::function<void()> paramLambda;
-    std::function<juce::String(float, int)> floatLambda;
-    std::function<juce::String(int, int)> intLambda;
-
-    auto genParam = juce::AudioProcessorParameter::genericParameter;
-    auto inMeter = juce::AudioProcessorParameter::inputMeter;
-    auto outParam = juce::AudioProcessorParameter::outputGain;
-    auto outMeter = juce::AudioProcessorParameter::outputMeter;
+    const auto genParam = juce::AudioProcessorParameter::genericParameter;
+    const auto inMeter = juce::AudioProcessorParameter::inputMeter;
+    const auto outParam = juce::AudioProcessorParameter::outputGain;
+    const auto outMeter = juce::AudioProcessorParameter::outputMeter;
     
     juce::ignoreUnused(inMeter);
+    juce::ignoreUnused(outParam);
     juce::ignoreUnused(outMeter);
 
     params.add
         //======================================================================
-        (std::make_unique<juce::AudioProcessorParameterGroup>("BandOneID", "0", "seperator",
-            //==================================================================
-            std::make_unique<juce::AudioParameterFloat>("frequencyID", "Frequency", freqRange, 632.455f, frequency, genParam),
-            std::make_unique<juce::AudioParameterFloat>("resonanceID", "Resonance", resRange, 00.10f, reso, genParam),
-            std::make_unique<juce::AudioParameterFloat>("gainID", "Gain", gainRange, 00.00f, decibels),
-            std::make_unique<juce::AudioParameterChoice>("typeID", "Type", fString, 0)
-            //==================================================================
-            ));
-
-    params.add
-        //======================================================================
-        (std::make_unique<juce::AudioProcessorParameterGroup>("masterID", "1", "seperator",
+        (std::make_unique<juce::AudioProcessorParameterGroup>("masterID", "0", "seperator0",
             //==================================================================
             std::make_unique<juce::AudioParameterChoice>("osID", "Oversampling", osString, 0),
-            std::make_unique<juce::AudioParameterFloat>("outputID", "Output", outputRange, 00.00f, decibels, outParam),
+            std::make_unique<juce::AudioParameterFloat>("outputID", "Output", outputRange, 00.00f, decibels, genParam),
             std::make_unique<juce::AudioParameterFloat>("mixID", "Mix", mixRange, 100.00f, percentage, genParam),
-            std::make_unique<juce::AudioParameterBool>("bypassID", "Bypass", false)
+            std::make_unique<juce::AudioParameterChoice>("precisionID", "Precision", pString, 0),
+            std::make_unique<juce::AudioParameterBool>("bypassID", "Bypass", false),
+            std::make_unique<juce::ArrowButton>("Undo", 0.5f, juce::Colours::antiquewhite)
             //==================================================================
             ));
 }
